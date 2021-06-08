2cd95c0e3913d0ff520fabcdaa9c252fbc728fbf
Miguel Camba <miguel.camba@gmail.com>
2021-06-08 21:27:15 +0200

    Initial commit. It works

---
+++ locales/en.js
@@ -0,0 +1,4 @@
+export default {
+  'basic': 'Hello world',
+  'interpolated': 'Passed in: {val}'
+};
---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { addMessages, init, getLocaleFromNavigator /*, register */ } from 'svelte-intl-precompile';
+  import en from '../locales/en.js';
+  // @ts-ignore
+  addMessages('en', en);
+  // register('es', () => import('../../locales/en.js')); <-- use this approach if you want locales to be load lazily
+
+  init({
+    fallbackLocale: 'en',
+    initialLocale: getLocaleFromNavigator()
+  });
+	import { t } from 'svelte-intl-precompile'	
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>{$t('basic')}</p>
+	<p>{$t('interpolated', { values: { val: name } })}</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
